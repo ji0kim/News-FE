f35323e87db6b6f1bfa14d7240a60fecb7ab0d86
Jay <jiyoung26091212@gmail.com>
2022-04-19 23:18:08 +0100

    feat : Change create date format, only showing the date

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -8,7 +8,7 @@ const ArticleCard = ({ article }) => {
 			{/* <p className='article-card__body'>{article.body}</p> */}
 			<p>
 				<span className='article-card__topic'>{article.topic}</span>
-				<span className='article-card__date'>{article.created_at}</span>
+				<span className='article-card__date'>{article.created_at.match(/([0-9]){4}\-([0-9]){2}\-([0-9]){2}/)[0]}</span>
 			</p>
 		</a>
 	);
