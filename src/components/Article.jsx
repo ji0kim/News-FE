88b692a9e2842c1828e03bc1dae270e9921dbc19
Jay <jiyoung26091212@gmail.com>
2022-04-21 00:04:39 +0100

    feat : Add the vote sectionand  add the IncrementVote function

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,9 +4,13 @@ import { getArticleById } from '../utils/api';
 
 const Article = () => {
 	const [article, setArticle] = useState({});
-	const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+	const [vote, setVote] = useState(0);
 	const { article_id } = useParams();
 
+	const incrementVote = () => {
+		setVote(vote + 1);
+	};
 	useEffect(() => {
 		getArticleById(article_id).then((articleFromApi) => {
 			setArticle(articleFromApi);
@@ -18,15 +22,22 @@ const Article = () => {
 		return <p>Loading</p>;
 	}
 	return (
-		<section className='article-page'>
-			<h1 className='article-page__title'>{article.title}</h1>
-			<p className='article-page__author'>
-				<span>by</span> {article.author}
-			</p>
-			<span className='article-page__date'>{article.created_at}</span>
-			<p>{article.body}</p>
-			<span className='article-page__topic'>{article.topic}</span>
-		</section>
+		<>
+			<section className='article-page'>
+				<h1 className='article-page__title'>{article.title}</h1>
+				<p className='article-page__author'>
+					<span>by</span> {article.author}
+				</p>
+				<span className='article-page__date'>{article.created_at}</span>
+				<p>{article.body}</p>
+				<span className='article-page__topic'>{article.topic}</span>
+			</section>
+			<section className='vote'>
+				<button onClick={incrementVote}>+</button>
+				<button>-</button>
+				<p className='article-page__votes'>{vote}</p>
+			</section>
+		</>
 	);
 };
 
