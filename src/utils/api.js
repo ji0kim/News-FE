c9f9a3c23166c7fec5e3788cb48c4b2109559627
Jay <jiyoung26091212@gmail.com>
2022-04-20 15:08:14 +0100

    feat : Delete getArticles and use getArticlesByTopics with topic useParam.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
-
+  
 const articlesApi = axios.create({
 	baseURL: 'https://news-api-j.herokuapp.com/api',
 });
 
-export const getArticles = () => {
-  return articlesApi.get('/articles').then(({ data }) => {
-		return data.articles;
-	});
-};
 export const getTopics = () => {
 	return articlesApi.get('/topics').then(({ data }) => {
 		return data.topics;
 	});
+};
+export const getArticlesByTopics = (topic) => {
+	return articlesApi
+		.get(`/articles`, {
+			params: { topic },
+		})
+		.then(({ data }) => {
+			return data.articles;
+		});
 };
