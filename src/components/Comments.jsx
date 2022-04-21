f667e1da1f5464791a50001a1c3221f6249f70e8
Jay <jiyoung26091212@gmail.com>
2022-04-21 13:31:12 +0100

    feat : Show comment count on the top of the comments section

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,8 +1,11 @@
 import { formatDate } from '../utils/utils';
+import '../css/comments.css';
 const Comments = ({ comments }) => {
 	return (
-		<section className='comment'>
-			<h2>comment</h2>
+		<section className='comments'>
+			<h2 className='section-tit'>
+				Comments<span className='count-num'>{comments.length}</span>
+			</h2>
 			<ul>
 				{comments.map((comment) => {
 					return (
@@ -10,7 +13,7 @@ const Comments = ({ comments }) => {
 							<span className='comment-author'>{comment.author}</span>
 							<span className='comment-date'>{formatDate(comment.created_at)}</span>
 							<p className='comment-body'>{comment.body}</p>
-							<p className='comment-votes'>{comment.votes}</p>
+							{/* <p className='comment-votes'>{comment.votes}</p> */}
 						</li>
 					);
 				})}
